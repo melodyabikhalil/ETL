680e52f49fb073bf362660421a7790b3fda78954
Melody <melody.ak@hotmail.com>
2020-03-14 10:36:48 +0200

    Creates join query class

---
+++ ETL/Core/JoinQuery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETL.Core
+{
+    class JoinQuery
+    {
+        public string queryName { get; set; }
+        public string query { get; set; }
+        public Database database { get; set; }
+        
+        public JoinQuery(string queryName, string query, Database database)
+        {
+            this.database = database;
+            this.query = query;
+            this.queryName = queryName;
+        }
+    }
+}
